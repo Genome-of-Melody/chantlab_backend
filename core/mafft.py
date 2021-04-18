1d2713d03983120e2f5f480c1525e43333cb449f
kristina <k.szabova98@gmail.com>
2021-04-18 19:49:31 +0200

    Mafft wrapper

---
+++ core/mafft.py
@@ -0,0 +1,51 @@
+import subprocess
+
+class Mafft():
+
+    def __init__(self):
+        self._input = None
+        self._output = None
+        self._options = ['--quiet']
+        # this should eventually be removed
+        self._prefix = "wsl"
+
+
+    def set_input(self, file):
+        # add check if the file exists?
+        self._input = file
+
+    
+    def set_output(self, file):
+        self._output = file
+
+    
+    def add_option(flag, value=None):
+        self._options.append(flag)
+        if value:
+            self._options.append(value)
+
+
+    def set_prefix(prefix):
+        self._prefix = prefix
+
+
+    def run(self):
+        command = ""
+        command += self._prefix + " " if self._prefix else ""
+        command += "mafft "
+        command += " ".join(self._options) + " "
+        command += self._input + " " if self._input else ""
+        # the --out option doesn't seem to be working?
+        # command += "--out " + self._output if self._output else ""
+        process = subprocess.run(command, capture_output=True)
+        if process.stderr:
+            print(process.stderr)
+        elif process.stdout:
+            if self._output:
+                with open(self._output, 'w') as out:
+                    out.write(process.stdout)
+            else:
+                print(process.stdout)
+
+
+
