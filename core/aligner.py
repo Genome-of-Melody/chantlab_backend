cca3d28eb4b6cab0e0db39d902781976d3356097
lanzv <vojtech.lanz@gmail.com>
2026-08-19 19:52:26 +0200

    return chant ids instead of row indicies in alignment errors

--- core/aligner.py
+++ core/aligner.py
@@ -57,7 +57,7 @@ class Aligner():
             if not is_compatible:
                 text_syllables = cls._extend_text_to_volpiano([], volpiano_syllables)
                 error_sources.append(sources[i])
-                error_ids.append(i)
+                error_ids.append(ids[i])
             success_sources.append(sources[i])
             success_ids.append(ids[i])
             success_urls.append(urls[i])
@@ -238,8 +238,9 @@ class Aligner():
                     # found an error, the alignment will be run again
                     # finished = False
                     logging.error(str(e))
-                    error_sources.append(sources[id])
-                    error_ids.append(id)
+                    if id != -1:
+                        error_sources.append(sources[id])
+                        error_ids.append(ids[id])
 
             cls._cleanup(mafft_inputs_path)   # Comment out this cleanup to retain MAFFT output files
         if concatenated:
@@ -401,8 +402,9 @@ class Aligner():
                     # found an error, the alignment will be run again
                     # finished = False
                     logging.error(str(e))
-                    error_sources.append(sources[id])
-                    error_ids.append(id)
+                    if id != -1:
+                        error_sources.append(sources[id])
+                        error_ids.append(ids[id])
 
             cls._cleanup(mafft_inputs_path)
 
